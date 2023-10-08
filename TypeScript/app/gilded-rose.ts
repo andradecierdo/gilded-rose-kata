279d286b1624f3b8363f0607764505a1590f6a0d
Andrade Decierdo <andraded@codev.com>
2023-10-08 18:12:02 +0800

    Create enum for item names and create methods for updating the quality of each type of item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,6 +21,13 @@ export interface IGildedRose {
   updateQuality(): void
 }
 
+enum Enum {
+  Sulfuras = 'Sulfuras, Hand of Ragnaros',
+  AgedBrie = 'Aged Brie',
+  BackStagePassess = 'Backstage passes to a TAFKAL80ETC concert',
+  ConjuredManaCake = 'Conjured Mana Cake',
+}
+
 export class GildedRose implements IGildedRose {
   items: Array<Item>
 
@@ -30,33 +37,50 @@ export class GildedRose implements IGildedRose {
 
   updateQuality(): void {
     this.items.forEach(item => {
-      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+      if (item.name !== Enum.Sulfuras) {
         item.sellIn--
 
         switch (item.name) {
-          case 'Aged Brie':
-            const addition = item.sellIn < 0 ? 2 : 1
-            item.quality = (item.quality + addition) > 50 ? 50 : item.quality + addition
+          case Enum.AgedBrie:
+            GildedRose.updateAgedBrieItem(item)
             break
-          case 'Backstage passes to a TAFKAL80ETC concert':
-            if (item.sellIn < 0) {
-              item.quality = 0
-            } else if (item.quality < 50) {
-              const addition = item.sellIn < 5 ? 3 : item.sellIn < 10 ? 2 : 1
-              let newQuality = item.quality + addition
-              item.quality = newQuality > 50 ? 50 : newQuality
-            }
+          case Enum.BackStagePassess:
+            GildedRose.updateBackStagePassesItem(item)
             break
-          case 'Conjured Mana Cake':
-            const deduction = item.sellIn < 0 ? -4 : -2
-            item.quality = (item.quality + deduction) < 0 ? 0 : item.quality + deduction
+          case Enum.ConjuredManaCake:
+            GildedRose.updateConjuredManaCakeItem(item)
             break
           default:
-            // Normal items
-            const deduct = item.sellIn < 0 ? -2 : -1
-            item.quality = (item.quality + deduct) < 0 ? 0 : item.quality + deduct
+            GildedRose.updateRegularItem(item)
         }
       }
     })
+  }
+
+  private static updateAgedBrieItem(item: IItem): void {
+    const addition = item.sellIn < 0 ? 2 : 1
+    item.quality = (item.quality + addition) > 50 ? 50 : item.quality + addition
+  }
+
+  private static updateBackStagePassesItem(item: IItem): void {
+    if (item.sellIn < 0) {
+      item.quality = 0
+    } else if (item.quality < 50) {
+      const addition = item.sellIn < 5 ? 3 : item.sellIn < 10 ? 2 : 1
+      let newQuality = item.quality + addition
+      item.quality = newQuality > 50 ? 50 : newQuality
+    }
+  }
+
+  private static updateConjuredManaCakeItem(item: IItem): void {
+    const deduction = item.sellIn < 0 ? -4 : -2
+    const newQuality = item.quality + deduction
+    item.quality = newQuality < 0 ? 0 : newQuality
+  }
+
+  private static updateRegularItem(item: IItem): void {
+    const deduction = item.sellIn < 0 ? -2 : -1
+    const newQuality = item.quality + deduction
+    item.quality = newQuality < 0 ? 0 : newQuality
   }
 }
