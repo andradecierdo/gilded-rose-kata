2ab12d0e18dc4eda0876d590f6a696d6178e69f1
Andrade Decierdo <andraded@codev.com>
2023-10-08 19:32:29 +0800

    Update golden-master-text-test.ts based on the new implementation

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,17 +1,24 @@
-import { GildedRose, Item } from '@/services'
+import {
+  AgedBrieItem,
+  BackStagePassesItem,
+  ConjuredManaCakeItem,
+  GildedRose,
+  RegularItem,
+  SulfurasItem
+} from '@/services'
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20), //
-  new Item("Aged Brie", 2, 0), //
-  new Item("Elixir of the Mongoose", 5, 7), //
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new RegularItem({ name: "+5 Dexterity Vest", sellIn: 10, quality: 20 }),
+  new AgedBrieItem({ name: "Aged Brie", sellIn: 2, quality: 0 }),
+  new RegularItem({ name: "Elixir of the Mongoose", sellIn: 5, quality: 7 }),
+  new SulfurasItem({ name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 }),
+  new SulfurasItem({ name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 }),
+  new BackStagePassesItem({ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20 }),
+  new BackStagePassesItem({ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49 }),
+  new BackStagePassesItem({ name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49 }),
   // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
-
+  new ConjuredManaCakeItem({ name: "Conjured Mana Cake", sellIn: 3, quality: 6 })
+];
 
 const gildedRose = new GildedRose(items);
 
